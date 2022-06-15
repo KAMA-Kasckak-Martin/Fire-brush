21bd84c4d1f36567ed56d871ee810865fbc90fc1
Martin Kascak <m.kascak93@gmail.com>
2022-06-15 17:04:17 +0200

    feat: finish

--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
 function setup() {
   createCanvas(400, 400);
-  background(220);
 }
-let diameter = random(10, 30);
-let opacity = random(10, 40);
-let green = 0;
 
 function draw() {
-  fill(random());
+  background("white");
   if (mouseIsPressed) {
+    let diameter = random(10, 30);
+    let opacity = 100;
+    let green = 0;
+
+    if (diameter < 20) {
+      opacity = random(10, 40);
+    } else {
+      green = random(100, 200);
+      opacity = random(40, 70);
+    }
+
+    fill(random(200, 255), green, 0, opacity);
+    let x = mouseX + random(-30, 25);
+    let y = mouseY + random(-25, 38);
+    circle(x, y, diameter);
   }
 }
