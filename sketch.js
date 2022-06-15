e8a6f496e497510e32b920b9b74e22714b6d2df6
Martin Kascak <m.kascak93@gmail.com>
2022-06-15 16:40:12 +0200

    feat: add variables

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
+let diameter = random(10, 30);
+let opacity = random(10, 40);
+let green = 0;
 
 function draw() {
-  background(220);
+  fill;
+  if (mouseIsPressed) {
+  }
 }
